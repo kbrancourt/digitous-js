afb5cc31f9d71a6c56a8c56c7b529e173bbe011e
Kitty BRANCOURT <k.brancourt@gmail.com>
2020-12-07 16:20:23 +0100

    Jour 1 en cours

---
+++ jour1.js
@@ -0,0 +1,31 @@
+//Hello World
+console.log('Hello World !');
+
+//string
+var test = "My name is Kitty";
+console.log(test);
+
+//concaténation
+var name = "Kitty";
+console.log("Nice to meet you " + name);
+
+//string length
+var testLength = "I'm very long !";
+console.log(testLength.length);
+
+//replace
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good"));
+
+//up and down
+var basic = "This is cool";
+console.log(basic);
+
+console.log(basic.toUpperCase());
+
+console.log(basic.toLowerCase());
+
+//split
+var world = "banana";
+var letters = world.split('');
+console.log(letter.charAt(1));
