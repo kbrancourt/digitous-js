307c021f30fa637f435c19817af60badeebd5cfd
Kitty BRANCOURT <k.brancourt@gmail.com>
2020-12-08 15:26:42 +0100

    js: exercice 2 (bonus)

--- jour2.js
+++ jour2.js
@@ -8,7 +8,7 @@ console.log(float);
 //convert
 var basic = 34;
 var stringified = basic.toString();
-console.log(stringified.toString());
+console.log(stringified);
 
 //round
 var num = 1.5;
@@ -20,9 +20,118 @@ var test = 12;
 var bis = 5;
 console.log(test + bis);
 
+var test = 12;
+var bis = 5;
+console.log(test - bis);
+
+var test = 12;
+var bis = 5;
+console.log(test * bis);
+
+var test = 12;
+var bis = 5;
+console.log(test / bis);
+
+var test = 12;
+var bis = 5;
+console.log(test ** bis);
+
+var test = 12;
+var bis = 5;
+console.log(test % bis);
+
 //comparaison
 var test = 143;
 var bis = 219;
 console.log( test > bis);
 
+var test = 143;
+var bis = 219;
+console.log( test >= bis);
+
+var test = 143;
+var bis = 219;
+console.log( test < bis);
+
+var test = 143;
+var bis = 219;
+console.log( test =< bis);
+
+var test = 143;
+var bis = 219;
+console.log( test == bis);
+
+var test = 143;
+var bis = 219;
+console.log( test != bis);
+
+var test = 143;
+var bis = 219;
+console.log( test !== bis);
+
+
 //condition
+var limit = 50;
+var score = 30;
+if (score >=50){
+    console.log("OK good!")
+} else{
+    console.log("Oh noo..")
+}
+
+//condition 2
+var password = "azerty";
+if (password.length>5){
+    console.log("The password is secure")
+}
+
+//condition 3
+var limit = 50;
+var score = 51;
+var password = "azerty";
+if(score>=limit && password.length>5){
+    console.log("Everything is good");
+} else if (score>=limit || password.length>5){
+    console.log("Something is good");
+} else if (score<limit && password.length>5){
+    console.log("Nothing is good");
+}
+
+var limit = 50;
+var score = 30;
+var password = "azerty";
+if(score>=limit && password.length>5){
+    console.log("Everything is good");
+} else if (score>=limit || password.length>5){
+    console.log("Something is good");
+} else if (score<limit && password.length>5){
+    console.log("Nothing is good");
+}
+
+var limit =64;
+var score = 55;
+var password = "azerty";
+if(score>=limit && password.length>5){
+    console.log("Everything is good");
+} else if (score>=limit || password.length<5){
+    console.log("Something is good");
+} else{
+    console.log("Nothing is good");
+}
+
+//bonus
+var random = 4;
+if(random=6){
+    console.log("Yes I win !")
+}else{
+    console.log("So close...")
+}
+
+var random = 0;
+if(random===6){
+    console.log("Yes I win !")
+}else{
+    console.log("So close...")
+}
+
+//bonus2
