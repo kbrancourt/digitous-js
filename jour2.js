b6379ad902906d91b5caa2acfef64fa1411778f8
Kitty BRANCOURT <k.brancourt@gmail.com>
2020-12-08 14:16:11 +0100

    js: exercice 2 (en cours)

---
+++ jour2.js
@@ -0,0 +1,28 @@
+//number
+var integer = 102;
+console.log(integer);
+
+var float = 13.9
+console.log(float);
+
+//convert
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified.toString());
+
+//round
+var num = 1.5;
+var rounded = (Math.round(1.5));
+console.log(rounded);
+
+//arithmetique
+var test = 12;
+var bis = 5;
+console.log(test + bis);
+
+//comparaison
+var test = 143;
+var bis = 219;
+console.log( test > bis);
+
+//condition
