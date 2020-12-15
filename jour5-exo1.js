f0b1be902e1cef6f5338aaa043dc6ca093a19dc4
Kitty BRANCOURT <k.brancourt@gmail.com>
2020-12-15 17:13:48 +0100

    js: jour5-exo1

--- jour5-exo1.js
+++ jour5-exo1.js
@@ -18,25 +18,16 @@ function calculate(num1,operator,num2){
     }else if(operator === "%"){
         return number1 % number2;
 
-    }else{
-        console.log("error");
     }
 }
-console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+
+
+if(process.argv.length === 5){
+    console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+} else{
+    console.log("Error");
+}
 
 calculate(5,"+", 5);
 
-
-
-//table
-function multiply(num){
-    var number= parseInt(num);
-    for(var i= 1; i<= 10; i++){
-        console.log( i +"x"+ number+"="+ number*i)
-    }
-    return number;
-}
-
-console.log(multiply(process.argv[2]));
-multiply(2)
 
