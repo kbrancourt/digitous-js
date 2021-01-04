8105e45d6398f40c9c4619968f03d059ac85bf95
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-01-04 16:04:04 +0100

    exo 7

--- jour7.js
+++ jour7.js
@@ -1,9 +1,52 @@
-var fs = require("fs");
+// var fs = require("fs");
 
-fs.readFile("jour7.txt" , function(err, data){
-    if (err){
-        console.error(err);
-        return;
-    }
-    console.log(data.toString())
-})
+// //file system
+// fs.readFile("jour7.txt" , function(err, data){
+//     if (err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data.toString())
+// });
+
+//map double
+var array = [1, 2, 3, 4 ,5];
+
+var double = array.map(function(num){
+    return num * 2;
+});
+console.log(double);
+
+//map names
+
+var longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+];
+var shortNames = longNames.map(function(person){
+    var newName = {
+        name: person.firstName + " " + person.lastName
+    };
+    return newName
+
+    // var shortNames = longNames.map((function (element) {
+    //     return {
+    //         name: element.firstName + " " + element.lastName
+    //     }
+    // }))
+});
+
+console.log(shortNames);
+
+//filters numbers
+var array = [1, "toto", 34, "javascript", 8];
+var numbers = array.filter(function(element){
+    return element.num;
+});
+console.log(numbers)
